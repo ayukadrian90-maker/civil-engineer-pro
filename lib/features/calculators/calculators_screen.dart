9025c97b71fd7e08333bbb26726a967ad35487f2
ayukadrian90-maker <ayukadrian90@gmail.com>
2026-07-13 10:16:52 +0100

    Update calculators_screen.dart

--- lib/features/calculators/calculators_screen.dart
+++ lib/features/calculators/calculators_screen.dart
@@ -1,139 +1,32 @@
+void calculate() {
 
-import 'package:flutter/material.dart';
-import 'perimeter/perimeter_screen.dart';
+  double a = double.tryParse(sideA.text) ?? 0;
+  double b = double.tryParse(sideB.text) ?? 0;
 
-class CalculatorsScreen extends StatelessWidget {
-  const CalculatorsScreen({super.key});
+  double perimeter = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Engineering Calculators'),
-      ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(16),
+  if (shape == 'Square') {
 
-        child: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 12,
-          mainAxisSpacing: 12,
+    perimeter = 4 * a;
 
-          children: [
+  } 
 
-            _calculatorCard(
-              context,
-              '📐 Perimeter',
-              Icons.straighten,
-              const PerimeterScreen(),
-            ),
+  else if (shape == 'Rectangle') {
 
-            _calculatorCard(
-              context,
-              '📏 Area',
-              Icons.square_foot,
-              null,
-            ),
+    perimeter = 2 * (a + b);
 
-            _calculatorCard(
-              context,
-              '🧱 Volume',
-              Icons.view_in_ar,
-              null,
-            ),
-
-            _calculatorCard(
-              context,
-              '🧱 Concrete',
-              Icons.foundation,
-              null,
-            ),
-
-            _calculatorCard(
-              context,
-              '🔩 Reinforcement',
-              Icons.build,
-              null,
-            ),
-
-            _calculatorCard(
-              context,
-              '🪵 Formwork',
-              Icons.architecture,
-              null,
-            ),
-
-            _calculatorCard(
-              context,
-              '🏠 Foundation',
-              Icons.home_work,
-              null,
-            ),
-
-            _calculatorCard(
-              context,
-              '🪨 Earthworks',
-              Icons.terrain,
-              null,
-            ),
-          ],
-        ),
-      ),
-    );
   }
 
 
-  Widget _calculatorCard(
-    BuildContext context,
-    String title,
-    IconData icon,
-    Widget? page,
-  ) {
+  setState(() {
 
-    return Card(
-      elevation: 3,
-
-      child: InkWell(
-
-        onTap: () {
-
-          if (page != null) {
-
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => page,
-              ),
-            );
-
-          }
-
-        },
-
-        child: Column(
-          mainAxisAlignment:
-              MainAxisAlignment.center,
-
-          children: [
-
-            Icon(
-              icon,
-              size: 40,
-            ),
-
-            const SizedBox(
-              height: 10,
-            ),
-
-            Text(
-              title,
-              textAlign: TextAlign.center,
-            ),
-
-          ],
-        ),
-      ),
+    calculation = PerimeterModel(
+      shape: shape,
+      value: perimeter,
+      unit: 'm',
     );
-  }
+
+  });
+
 }
